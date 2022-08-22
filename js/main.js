337a9cd2d9a2cb2f5ad2967291c785c4326c72b9
Def <deferenseo@gmail.com>
2022-08-22 19:15:12 +0300

    fixed nav--mobile 1000px

--- js/main.js
+++ js/main.js
@@ -18,14 +18,13 @@ $('.slider__btn--next').click(function() {
 
 
 // Nav icon
-/* document.querySelector('.menu-icon-wrapper').onclick = function() {
-    document.querySelector('.menu-icon').classList.toggle('menu-icon-active');
-}; */
+
+const navBtn = document.querySelector('.nav__toggle');
+const nav = document.querySelector('.nav');
+const menuIcon = document.querySelector('.menu-icon');
 
 
-let iconWrapper = document.querySelector('.menu-icon-wrapper');
-let menuIcon = document.querySelector('.menu-icon');
-
-iconWrapper.addEventListener('click', () => {
+navBtn.addEventListener('click', () => {
+    nav.classList.toggle('nav--mobile');
     menuIcon.classList.toggle('menu-icon-active');
 });
